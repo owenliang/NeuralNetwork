4fad30b89593e63c52bef826bcebe35fe003fb7e
owenliang <owenliang1990@gmail.com>
2019-04-08 21:20:03 +0800

    update

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-rm -rf mnist_dataset && mkdir -p mnist_dataset
-cd mnist_dataset
+rm -rf mnist_dataset && mkdir -p mnist_dataset && cd mnist_dataset
 wget https://pjreddie.com/media/files/mnist_train.csv
 wget https://pjreddie.com/media/files/mnist_test.csv
 wget https://raw.githubusercontent.com/makeyourownneuralnetwork/makeyourownneuralnetwork/master/mnist_dataset/mnist_train_100.csv
